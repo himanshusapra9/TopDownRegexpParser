b620dc5c3cbe81488037708c194700afffb9e8c2
Anton Kovsharov <antonkovsharov@gmail.com>
2014-04-05 18:55:00 +0400

    LexicalAnalyzer added:

---
+++ src/LexicalAnalyzer.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+
+/**
+ * Created by antonkov on 4/5/14.
+ */
+public class LexicalAnalyzer {
+    InputStream is;
+    int curChar;
+    int curPos;
+    Token curToken;
+
+    public LexicalAnalyzer(InputStream is) throws ParseException {
+        this.is = is;
+        curPos = 0;
+        nextChar();
+    }
+
+    private boolean isBlank(int c) {
+        return c == ' ' || c == '\r' || c == '\n' || c == '\t';
+    }
+
+    private void nextChar() throws ParseException {
+        curPos++;
+        try {
+            curChar = is.read();
+        } catch (IOException e) {
+            throw new ParseException(e.getMessage(), curPos);
+        }
+    }
+
+    public void nextToken() throws ParseException {
+        while (isBlank(curChar)) {
+            nextChar();
+        }
+        if (curChar == '(') {
+            nextChar();
+            curToken = Token.LPAREN;
+        } else if (curChar == ')') {
+            nextChar();
+            curToken = Token.RPAREN;
+        } else if (curChar == '*') {
+            nextChar();
+            curToken = Token.STAR;
+        } else if (curChar == '|') {
+            nextChar();
+            curToken = Token.ALT;
+        } else if (curChar >= 'a' && curChar <= 'z') {
+            nextChar();
+            curToken = Token.LETTER;
+        } else if (curChar == -1) {
+            curToken = Token.END;
+        } else {
+            throw new ParseException("Illegal character " + (char) curChar, curPos);
+        }
+    }
+
+    public Token curToken() {
+        return curToken;
+    }
+
+    public int curPos() {
+        return curPos;
+    }
+}
